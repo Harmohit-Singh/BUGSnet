55756867e224dee5d0dc083893402146b1d41c07
Harmohit-Singh <harmohitsinghbindra@gmail.com>
2020-12-09 18:20:27 -0500

    exp

--- tests/testthat/Survival.R
+++ tests/testthat/Survival.R
@@ -11,13 +11,14 @@ test_that("Data Preparation",
             expect_that(rate2.slr, is_a("BUGSnetData"))
           })
 
-network.char <- net.tab(data = rate2.slr,
-                        outcome = "diabetes",
-                        N = "n",
-                        type.outcome = "rate2",
-                        time = "followup")
+
 
 test_that("net.tab",
           {
+            network.char <- net.tab(data = rate2.slr,
+                                    outcome = "diabetes",
+                                    N = "n",
+                                    type.outcome = "rate2",
+                                    time = "followup")
             expect_true(nrow(network.char$network)==13)
           })
