5647e346384b38026801155a181fc4d85265de5e
Harmohit-Singh <harmohitsinghbindra@gmail.com>
2020-12-09 18:11:26 -0500

    Exp

--- tests/testthat/Survival.R
+++ tests/testthat/Survival.R
@@ -1,11 +1,23 @@
 library(testthat)
 library(BUGSnet)
 
+data(diabetes.sim)
+rate2.slr <- data.prep(arm.data = diabetes.sim,
+                       varname.t = "Treatment",
+                       varname.s = "Study")
+
 test_that("Data Preparation",
           {
-            data(diabetes.sim)
-            rate2.slr <- data.prep(arm.data = diabetes.sim,
-                                   varname.t = "Treatment",
-                                   varname.s = "Study")
             expect_that(rate2.slr, is_a("BUGSnetData"))
+          })
+
+network.char <- net.tab(data = rate2.slr,
+                        outcome = "diabetes",
+                        N = "n",
+                        type.outcome = "rate2",
+                        time = "followup")
+
+test_that("net.tab",
+          {
+            expect_true(nrow(network.char$network)==13)
           })
